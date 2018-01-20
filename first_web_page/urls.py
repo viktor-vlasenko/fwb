436f5b8a06ce923b5a57a5b590e169c3b7b35789
viktor-vlasenko <viktor.vlasenko@phystech.edu>
2018-01-21 01:38:10 +0300

    third

--- first_web_page/urls.py
+++ first_web_page/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from core.views import index
+from core.views import index, topic_details
 from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^index/$', index),
+    url(r'^topic/(?P<pk>\d+)/$', topic_details, name='topic_details'),
     url(r'^admin/', admin.site.urls),
 ]
